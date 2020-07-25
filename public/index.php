1ef525cb1e144b45a4af1d779978cac5cd9a0f47
Skip <skiphog@gmail.com>
2020-07-25 16:25:54 +0300

    Initial commit-3

--- public/index.php
+++ public/index.php
@@ -2,4 +2,19 @@
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-var_dump($_SERVER);
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Документ</title>
+</head>
+<body>
+<div class="container mt-5">
+    <button class="btn btn-primary">Очень длинная кнопка для нажатия</button>
+</div>
+</body>
+</html>
